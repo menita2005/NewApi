671b456022fc6802118ae783dcedb4a0fcdde128
menita2005 <menaloaizasebastian@gmail.com>
2023-10-24 17:27:21 -0500

    Ultimos Cambios con El instructor Mapeando las clases

---
+++ NewApi/NewApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using NewApi.Models;
+
+namespace NewApi.Data
+{
+   
+
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options)
+            {
+
+            }
+
+            public DbSet<Supplier> Suppliers { get; set; }
+            public DbSet<Customer> Customers { get; set; }
+            public DbSet<Order> Orders { get; set; }
+            public DbSet<OrderItem> OrderItems { get; set; }
+            public DbSet<Product> Products { get; set; }
+        
+    }
+}
