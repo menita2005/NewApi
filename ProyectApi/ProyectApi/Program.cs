74942e40728b25507c4bc5c404b6cba278346df4
Snnat19 <jnatsanchezrr@gmail.com>
2023-12-05 17:31:57 -0500

    Public api

--- ProyectApi/ProyectApi/Program.cs
+++ ProyectApi/ProyectApi/Program.cs
@@ -34,6 +34,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
